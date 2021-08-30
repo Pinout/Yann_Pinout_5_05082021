6f0f2900e687d6a048242994c63b70ce8c358d8b
Pinout <yann.pinout@hotmail.fr>
2021-08-30 13:01:11 +0200

    J'ai réussi à accéder à l'API + affichage des teddies sur la page d'accueil réussi

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
@@ -12,12 +11,14 @@ const furnitureRoutes = require('./routes/furniture');
 const Teddy = require('./models/Teddy');
 
 const app = express();
+ 
 
-mongoose.connect('mongodb+srv://Yann:H1MJOutHwLM9lluQ@cluster0.ayvcw.mongodb.net/Cluster0?retryWrites=true&w=majority',
+
+/*mongoose.connect('mongodb+srv://Yann:H1MJOutHwLM9lluQ@cluster0.ayvcw.mongodb.net/Cluster0?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch(() => console.log('Connexion à MongoDB échouée !'));*/
 
 app.set('view engine', 'html');
 
@@ -26,6 +27,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   //res.set("Content-Security-Policy", "default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;");
+  /*res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    " font-src 'self'; img-src 'self' data: ; script-src 'self' data: ; style-src-elem 'self' data: ; style-src 'self' data: ; frame-src 'self';"
+  );*/
   next();
 });
 
@@ -33,48 +38,23 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/views'));
 express.static('views')
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy-Report-Only',
-    " font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css; frame-src 'self';"
-  );
-  next();
-});
-
-
-
-/*
-app.post('/api/teddies', (req, res, next) => {
-  delete req.body._id;
-  const teddy = new Teddy({
-    ...req.body
-  });
-  teddy.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 //On récupère tous les teddies
-app.use('/api/teddies', (req, res, next) => {
+app.get('/api/teddies', (req, res, next) => {
   Teddy.find()
     .then(teddies => res.status(200).json(teddies))
     .catch(error => res.status(400).json({ error }));
 });
 //On récupère 1 teddy
 app.get('/api/teddies/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
+  Teddy.findById({ _id: req.params.id })
     .then(teddy => res.status(200).json(teddy))
     .catch(error => res.status(404).json({ error }));
 });
-*/
-
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
-
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(helmet());
 
 app.use('/api/cameras', cameraRoutes);
 app.use('/api/teddies', teddyRoutes);
--- views/js/index.js
+++ views/js/index.js
@@ -1,4 +1,4 @@
-let container = document.getElementById("container");
+/*let container = document.getElementById("container");
 
 class Teddy {
     constructor({
@@ -19,36 +19,107 @@ class Teddy {
         this.quantity = parseInt(quantity, 10); // transforme chaine de caractère en nombre
     }
 };
-const display = teddy => {
-    container.innerHTML += `
-    <article id="cardsProduct" class="produit">
-        <img src=${teddy.imageUrl} alt="photos produits" />
-        <div class="bloqueDescription">
-            <h2> ${teddy.name}</h2>
-            <p>${teddy.price / 100}€</p>
-        </div>
-        <p>${teddy.description}</p>
-        <a href="produit.html?id=${teddy.id}"> En savoir plus</a>
-    </article>`
-};
+*/
+
+//Récupération de l'id pour rediriger vers la page product correspondante
+function getUrlProduct(teddies,i,linkProduct) {
+   
+    // récupération de l'url
+    let splitUrl = window.location.pathname.split("/");
+    let lastItem = splitUrl.pop();
+    // console.log(window.location.pathname.replace(lastItem, 'product.html'))
+    let url = window.location.origin + window.location.pathname.replace(lastItem, '../produit.html');
+
+    // // Création d'un objet url
+    let urlObj = new URL(url);
+    let idTeddies = teddies[i]._id;
+    // Ajout du query string id
+    urlObj.searchParams.append("id", idTeddies);
+    linkProduct.href = urlObj;
+}
+
+//Création du bouton de redirection avec le bon url
+function createButtonLinkProduct(linkProduct) {
+    let buttonBuy = document.createElement("button");
+    linkProduct.appendChild(buttonBuy);
+    buttonBuy.classList.add("btn", "btn-warning", "block-right");
+    // Ajout texte au bouton
+    buttonBuy.textContent = "Voir";
+}
+
+function createCardTeddies(teddies) {
+    let divParentParent = document.createElement("div");
+    const mainHome = document.getElementById("main");
+    mainHome.appendChild(divParentParent);
+    divParentParent.classList.add("row-cols-1", "row-cols-md-4", "row-cols-lg-5", "d-flex", "flex-wrap", "justify-content-between", "align-items-between");
+
+    for (let i = 0; i < teddies.length; i++) {
+
+        let divParent = document.createElement("div");
+        divParentParent.appendChild(divParent);
+        divParent.classList.add("card", "col", "m-3", "pt-3");
+        divParent.style.backgroundColor="rgb(183 116 138)";
+
+        // Création des élements images et div avec la classe card body, enfants de divParent 
+        let imageTeddy = document.createElement("img");
+        divParent.appendChild(imageTeddy);
+        imageTeddy.classList.add("card-image-top", "photo", "img-fluid");
+        imageTeddy.src = teddies[i].imageUrl;
+
+        let divCardBody = document.createElement("div");
+        divParent.appendChild(divCardBody);
+        divCardBody.classList.add("card-body", "text-center", "px-0", "d-flex", "flex-column", "justify-content-between");
+
+
+        // Création des éléments enfants de divCardBody
+        let titleTeddy = document.createElement("h3");
+        divCardBody.appendChild(titleTeddy);
+        titleTeddy.classList.add("card-title", "title");
+        titleTeddy.textContent = teddies[i].name;
+
+        let descriptionTeddy = document.createElement("p");
+        divCardBody.appendChild(descriptionTeddy);
+        descriptionTeddy.classList.add("description", "text-justify");
+        descriptionTeddy.textContent = teddies[i].description;
+
+
+        // Div prix + bouton
+        let divLinkPrice = document.createElement("div");
+        divCardBody.appendChild(divLinkPrice);
+        divLinkPrice.classList.add("d-flex", "flex-row", "justify-content-between");
+
+        // Prix
+        let priceTeddy = document.createElement("p");
+        divLinkPrice.appendChild(priceTeddy);
+        priceTeddy.classList.add("price", "my-2", "font-weight-bold");
+        priceTeddy.textContent = teddies[i].price + ' $';
+
+        let linkProduct = document.createElement("a");
+        divLinkPrice.appendChild(linkProduct);
+        getUrlProduct(teddies,i,linkProduct);
+        createButtonLinkProduct(linkProduct);
+    }
+}
+
 
 //APPEL API AVEC FETCH
-fetch("https://teddies-api.herokuapp.com/api/teddies")
-    .then(response => response.json())  
-    .then(function (listeProduct) {
-        // boucle for prend un produit de la liste 
-        for (let product of listeProduct) {
-            let teddy = new Teddy(product)
-            display(teddy);
+async function getTeddies() {
+    try {
+        let response = await fetch("https://teddies-api.herokuapp.com/api/teddies");
+        if (response.ok) {
+            let teddies = await response.json();
+            createCardTeddies(teddies);
+        } else {
+            console.error('Retour du serveur : ', response.status)
         }
-    })
-    //SI PROBLEME API
-    .catch(function (err) {
-        console.log("fetch Error")
-        alert("Veuillez nous exuser les produits ne sont pas disponibles pour le moment ")
-    });
+    } catch (e) {
+        console.log(e);
+    }
+}
+getTeddies()
 
-function convertPrice(productPrice) {
+
+/*function convertPrice(productPrice) {
     let price = `${productPrice}`;
     price = Intl.NumberFormat("fr-FR", {
         style: "currency",
@@ -56,8 +127,8 @@ function convertPrice(productPrice) {
         minimumFractionDigits: 2,
     }).format(price / 100);
     return price;
-}
-function addCards(data) {
+}*/
+/*function addCards(data) {
     //boucle pour chaque iteration d'un produit
     for (produit of data) {
         //recupère l'élément liste dans le HTML
@@ -83,4 +154,4 @@ function addCards(data) {
           </div>
       </div>`;
     }
-}
+}*/
